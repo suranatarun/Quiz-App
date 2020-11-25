a1512f06c1546ea52e1d02a5d44a32641e7aeca4
Tarun Surana <52948930+suranatarun@users.noreply.github.com>
2020-11-25 18:24:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WelcomeHomeComponent } from './welcome-home/welcome-home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    WelcomeHomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'Home', component: HomeComponent },
+      { path: '', redirectTo: 'Home', pathMatch: 'full' },
+      { path: 'Home/Login', component: LoginComponent },
+      { path: 'Home/Sign-Up', component: SignUpComponent },
+      { path: 'Home/Welcome-Home', component: WelcomeHomeComponent},
+      { path: '*', component: NotFoundComponent },
+      { path: '**', component: NotFoundComponent },
+      { path: '***', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/welcome-home/welcome-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-home',
+  templateUrl: './welcome-home.component.html',
+  styleUrls: ['./welcome-home.component.css']
+})
+export class WelcomeHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
